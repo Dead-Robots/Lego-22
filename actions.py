601b581a7cc27edc15977858297a356ce66f5f32
Libby McClough <libbymcclough26@outlook.com>
2022-03-17 20:40:26 -0400

    Create routine to obtain and deliver rings.

--- actions.py
+++ actions.py
@@ -2,17 +2,67 @@ import drive
 from drive import drive_straight, pivot, spin, drive, freeze_bot, drive_diagonal
 import constants as c
 from elevator import move_timed
-from kipr import motor, freeze, ao, msleep, motor_power, get_motor_position_counter, clear_motor_position_counter, enable_servos, disable_servos
+from kipr import motor, freeze, ao, msleep, motor_power, get_motor_position_counter, clear_motor_position_counter, \
+    enable_servos, disable_servos
 import servos as s
+
 
 def init():
     print("Starting up")
     enable_servos()
-   # drive_straight(30, 5)
-   # right_pivot(30, 1000)
+    # drive_straight(30, 5)
+    # right_pivot(30, 1000)
 
-def move_wrist_left(position, speed):
-    s.move_servo(c.WRIST, position, speed)
+
+def start_position():
+    print("moving to start position")
+    s.move_servo(c.ARM, c.ARM_TO_RIGHT)
+    msleep(100)
+    s.move_servo(c.WRIST, c.WRIST_PARALLEL)
+    msleep(100)
+
+
+def get_rings(distance, height):
+    drive_straight(-70, 6)
+    msleep(100)
+    s.move_servo(c.ARM, c.ARM_TO_MIDDLE)
+    # lift elevator to height
+    # position elevator at height 0 level to pick up top 3 rings
+    drive_straight(50, distance)  # drive forward
+    msleep(500)
+    drive_straight(-50, distance)
+    # move_elevator_up(35, height)
+
+
+def deliver_rings():
+    s.move_servo(c.ARM, 1340)  # moves arm so it doesn't hit the side pvc pipe
+    msleep(100)
+    drive_straight(-70, 29)
+    # can use to gyro to go until bump instead
+    s.move_servo(c.ARM, c.ARM_TO_LEFT)
+    s.move_servo(c.WRIST, c.WRIST_TO_LEFT)
+    # move forward to place rings on tube
+    drive_straight(70, 6)
+    msleep(500)
+    drive_straight(-70, 6)
+    msleep(500)
+    s.move_servo(c.ARM, 1340)  # moves arm so it doesn't hit the side pvc pipe
+    msleep(500)
+
+
+def return_to_rings(distance):
+    s.move_servo(c.WRIST, c.WRIST_PARALLEL)
+    msleep(100)
+    drive_straight(70, 29)
+    msleep(100)
+    s.move_servo(c.ARM, c.ARM_TO_MIDDLE)
+    drive_straight(50, distance)  # drive forward
+    msleep(500)
+    drive_straight(-50, distance)
+
+
+def move_wrist_left(position):
+    s.move_servo(c.WRIST, position)
     freeze(c.WRIST)
 
 
@@ -32,27 +82,6 @@ def move_elevator_up(power, height):
         motor_power(c.ELEVATOR, power)
         position = get_motor_position_counter(c.ELEVATOR)
     freeze(c.ELEVATOR)
-
-
-def get_rings(distance, height):
-    # lift elevator to height
-    move_elevator_down(35, 0)
-    # position elevator at height 0 level to pick up top 3 rings
-    drive_straight(50, distance)  # drive forward
-    move_elevator_up(35, height)
-
-
-    freeze_bot()
-
-
-def deliver_rings():
-    drive_straight(-70, 35)
-    # turn arm
-    s.move_servo(c.ARM, 0)
-    # turn wrist
-    s.move_servo(c.WRIST, 0)
-    # move forward to place rings on tube
-    # turn to free claw from tube
 
 
 def elevator_up():
